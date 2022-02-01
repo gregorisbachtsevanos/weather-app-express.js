0f9b4f7aa992c7f582d8741cc365ec944537ad9f
GregorisB <94171068+GregorisB@users.noreply.github.com>
2022-02-01 16:27:40 +0200

    weather part1

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const functions = require('./utils/functions')
 const location = process.argv[2]
 
 if (location) {
-	functions.mapbox(location, (err, { location, latitude, longitude }) => {
-		err ? console.log(err) : console.log(location)
-		functions.weather(latitude, longitude, (err, { name, country }, { temperature, weather_descriptions, feelslike }) => {
-			err ? console.log(err) : console.log(name, country, temperature, weather_descriptions.toString(), feelslike)
+	functions.mapbox(location, (err, {
+		location,
+		latitude,
+		longitude
+	} = {}) => {
+		err ? console.log(err) : console.log(`Location: ${location}`)
+		functions.weather(latitude, longitude, (err, {
+			name,
+			country
+		}, {
+			temperature,
+			weather_descriptions,
+			feelslike
+		}) => {
+			err
+				? console.log(err)
+				: console.log(
+					`INFO`,
+					'\r\n' + `Country: ${country}`,
+					'\r\n' + `Temperature: ${temperature}`,
+					'\r\n' + `Weather: ${weather_descriptions.toString()}`,
+					'\r\n' + `Feeling: ${feelslike}`
+				)
 		})
 	})
-}else{
+} else {
 	console.log("Please provide some data.")
 }
--- utils/functions.js
+++ utils/functions.js
@@ -3,18 +3,23 @@ const request = require('request')
 const mapboxApi = "https://api.mapbox.com/geocoding/v5/mapbox.places/.json?access_token=pk.eyJ1IjoiZ3JlZ29yaXMiLCJhIjoiY2t6MWJhZXp6MGVqczJ3dDJrd3M4a2NxNSJ9.TEJyEZkK7v_07JUAr5Gvaw"
 const weatherApi = 'http://api.weatherstack.com/current?access_key=23a2b28c79e141772b403b91548625dc&query='
 const mapbox = (address, callback) => {
-	
+
 	let url = mapboxApi.slice(0, 50) + encodeURIComponent(address) + mapboxApi.slice(50)
-	request({ url, json: true }, (err, { body }) => {
-		if(err){
+	request({
+		url,
+		json: true
+	}, (err, {
+		body
+	}) => {
+		if (err) {
 			callback('Unable to connect to to location services')
-		}else if (body.features.length === 0){
+		} else if (body.features.length === 0) {
 			callback("Provide an existing city")
-		}else{
+		} else {
 			callback(undefined, {
 				longitude: body.features[0].geometry.coordinates[0],
 				latitude: body.features[0].geometry.coordinates[1],
-				location: body.features[0].place_name 
+				location: body.features[0].place_name
 			})
 		}
 	})
@@ -22,12 +27,17 @@ const mapbox = (address, callback) => {
 
 const weather = (latitude, longitude, callback) => {
 	const url = `${weatherApi}${latitude},${longitude}`
-	request({ url, json: true}, (err, { body }) => {
-		if(err){
+	request({
+		url,
+		json: true
+	}, (err, {
+		body
+	}) => {
+		if (err) {
 			callback('Unable to connect to to location services')
-		}else if (body.error){
+		} else if (body.error) {
 			callback(body.error.info)
-		}else{
+		} else {
 			callback(undefined, body.location, body.current)
 		}
 	})
