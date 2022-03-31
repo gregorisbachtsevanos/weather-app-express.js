fac428f7e831dfd98e709b4ff3d3969afd9b56dd
Gregoris Bachtsevanos <94171068+GregorisB@users.noreply.github.com>
2022-04-01 01:25:19 +0300

    Update functions.js

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,9 +1,7 @@
 const request = require('request')
 const mapboxApi = "https://api.mapbox.com/geocoding/v5/mapbox.places/.json?access_token=pk.eyJ1IjoiZ3JlZ29yaXMiLCJhIjoiY2t6MWJhZXp6MGVqczJ3dDJrd3M4a2NxNSJ9.TEJyEZkK7v_07JUAr5Gvaw"
-// main account
-// const weatherApi = 'http://api.weatherstack.com/current?access_key=23a2b28c79e141772b403b91548625dc&query='
-// fake account
-const weatherApi = 'http://api.weatherstack.com/current?access_key=175d1bc6e9e139b802035d627d5a2796&query='
+
+const weatherApi = 'http://api.weatherstack.com/current?access_key=23a2b28c79e141772b403b91548625dc&query='
 const mapbox = (address, callback) => {
 
 	let url = mapboxApi.slice(0, 50) + encodeURIComponent(address) + mapboxApi.slice(50)
